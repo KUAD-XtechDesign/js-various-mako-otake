7ea3d9e6cc4e455e4f047afdbbe6e624e761929b
mako-otake <68629547+mako-otake@users.noreply.github.com>
2020-11-13 18:03:41 +0900

    sample,js4変更

--- sample/js/script04.js
+++ sample/js/script04.js
@@ -5,3 +5,21 @@ $(function(){
     columnWidth: 200
   });
 })
+
+$(function(){
+
+  $("#intro p").hide().fadeIn(1000)
+  
+  $("#intro").on("click",introAnime)
+
+  function introAnime(){
+    $("#intro").fadeOut(1000,contentAnime);
+  }
+
+  function contentAnime(){
+    setTimeout(backAnime,1000);
+  }
+  
+  
+
+})
